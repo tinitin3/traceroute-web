0ada3a1152326cf72058ba66bbfadf1b19d3d1b0
tinitin3 <eliot.morteau@ikmail.com>
2025-02-23 17:09:50 +0100

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 import subprocess
 import re
 import requests
+import os
+
 
 app = Flask(__name__)
 CORS(app) 
@@ -38,4 +40,4 @@ def traceroute():
     return jsonify(geo_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
